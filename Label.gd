a95c318c8fd32506306ec9e9f6d8c3ea600fbd43
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-05-06 17:16:50 +0530

    (feat): move sprite around

--- Label.gd
+++ Label.gd
@@ -4,6 +4,16 @@ var ANGULAR_SPEED = PI
 var SPEED = 250.0
 
 func _process(delta):
-	rotation += ANGULAR_SPEED * delta
-	var velocity = Vector2.UP.rotated(rotation) * SPEED
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	elif Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += ANGULAR_SPEED * direction * delta
+
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * SPEED
 	position += velocity * delta

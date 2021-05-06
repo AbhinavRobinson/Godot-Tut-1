7d59e0c4ef27ef90befacb478ad7658d6a98fb99
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-05-06 18:09:52 +0530

    (feat): use signals

--- using-signals/CustomSignal/LifeBar.gd
+++ using-signals/CustomSignal/LifeBar.gd
@@ -1,1 +1,4 @@
 extends TextureProgress
+
+func _on_Player_health_changed(new_health):
+	value = new_health
--- using-signals/CustomSignal/Player.gd
+++ using-signals/CustomSignal/Player.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-var health = 10
+signal health_changed(new_health)
 
+var health = 10
 
 func take_damage(amount):
 	health -= amount
 	if health < 0:
 		health = 0
-
 	get_node("AnimationPlayer").play("take_damage")
-
+	emit_signal("health_changed", health)
 
 func _on_area_entered(area):
 	take_damage(2)

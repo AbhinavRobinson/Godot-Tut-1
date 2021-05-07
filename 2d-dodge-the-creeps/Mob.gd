48f98bd738038c79652bd57c375ff9d86921846d
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-07 15:36:01 +0530

    (feat): add mob destroy signal

--- 2d-dodge-the-creeps/Mob.gd
+++ 2d-dodge-the-creeps/Mob.gd
@@ -5,9 +5,15 @@ export var min_speed = 150.0
 export var max_speed = 250.0
 
 func _ready():
+	# randomize seed
+	randomize()
 	# play anim
 	$AnimatedSprite.play()
 	# get all mob types
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
 	# get random mob
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+# Destory monster when mob exits screen
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()

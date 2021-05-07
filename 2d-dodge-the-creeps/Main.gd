43d838203cbffec0002e17f9b2ca6989368d45aa
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-07 16:43:56 +0530

    (feat): add timers

--- 2d-dodge-the-creeps/Main.gd
+++ 2d-dodge-the-creeps/Main.gd
@@ -8,6 +8,23 @@ func _ready():
 	# randomize on start
 	randomize()
 
+func new_game():
+	score = 0
+	$HUD.update_score(score)
+	
+	$StartTimer.start()
+	
+	$HUD.show_message("Get Ready ...")
+	
+	yield($StartTimer, "timeout")
+	$ScoreTimer.start()
+	$MobTimer.start()
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+
 func _on_MobTimer_timeout():
 	# get mob spawn location
 	var mob_spawn_location = $MobPath/MobSpawnLocation
@@ -29,3 +46,8 @@ func _on_MobTimer_timeout():
 	# add velocity
 	var velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = velocity.rotated(direction)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)

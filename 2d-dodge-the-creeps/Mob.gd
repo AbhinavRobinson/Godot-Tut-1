a1542b70282dc317c3bf6d3d9c7e0d7322b13073
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-07 15:32:12 +0530

    (feat): add random mob types

--- 2d-dodge-the-creeps/Mob.gd
+++ 2d-dodge-the-creeps/Mob.gd
@@ -1,2 +1,13 @@
 extends RigidBody2D
 
+# Export min/max speed
+export var min_speed = 150.0
+export var max_speed = 250.0
+
+func _ready():
+	# play anim
+	$AnimatedSprite.play()
+	# get all mob types
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	# get random mob
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
